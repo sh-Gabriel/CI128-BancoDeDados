89e18d2b8348a96d32a008d81efb911195fd753b
Gabriel Silva Hermida <gash18@inf.ufpr.br>
2021-07-20 14:25:23 -0300

    Documentacao
    Signed-off-by: Gabriel Silva Hermida <gash18@inf.ufpr.br>

--- code/output.h
+++ code/output.h
@@ -6,6 +6,21 @@
 
 #define TAM_CMD 8
 
+/**
+ * @brief Calcula o tamanho em bytes que as chaves dos vértices, somadas, têm
+ * 
+ * @param lista A lista de vértices existente
+ * @return int O tamanho em bytes
+ */
 int tamanho_lista(VertexList *lista);
 
+/**
+ * @brief Formata e printa a output do programa
+ * 
+ * @param lista A lista de vértices do escalonamento corrente
+ * @param ident_escalonamento A id do escalonamento
+ * @param isSerial O escalonamento ser serial
+ * @param isEquivalent O escalonamento ser equivalente
+ * @param size_lista O tamanho em bytes da soma dos vértices da lista
+ */
 void print_output(VertexList *lista, int ident_escalonamento, bool isSerial, bool isEquivalent, int size_lista);
